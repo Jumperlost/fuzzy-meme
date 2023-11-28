b68acd9cf6b8eaa147ef50245ab4c3e073719bcc
Jumperlost <kosver00795@gmail.com>
2023-11-28 14:11:50 +0200

    added

--- js/masterJS.js
+++ js/masterJS.js
@@ -5,71 +5,110 @@ function myFunction() {
   firstAction.style.display = "none";
   secondAction.style.display = "block";
 }
-
+//Generations TABLE
 function createDynamicForm() {
   const participants = document.getElementById("participants").value;
   const juries = document.getElementById("juries").value;
   const dynamicFormContainer = document.getElementById("thirdpage");
+  // MAX NUMBER INPUT
+  const elem = document.getElementById("maxPoints").value;
+  //
   dynamicFormContainer.innerHTML = "";
   secondAction.style.display = "none";
 
   let dynamicFormHTML = '<div class="table-responsive-sm">';
-  dynamicFormHTML += "<table  class='table table-success'>";
-  dynamicFormHTML += "<thead>";
-  dynamicFormHTML += "</tr>";
-  dynamicFormHTML +=
-    "<th scope='col'><i class='fa-solid fa-people-group'></i></th>";
+  dynamicFormHTML += '<table class="table table-success">';
+  dynamicFormHTML += "<thead><h1 class='display-3'>leaderbord</h1>";
+  dynamicFormHTML += "<tr>";
+  dynamicFormHTML += '<th scope="col"></th>';
   for (let h = 0; h < juries; h++) {
-    dynamicFormHTML += `<td><input type="text" placeholder="
-    Name of the jury"></td>`;
+    dynamicFormHTML += `<th><input type="text" placeholder="Name of the jury"></th>`;
   }
-  dynamicFormHTML += "<th scope='col'>Total points</th>";
+
+  dynamicFormHTML += '<th scope="col">Total points</th>';
+  dynamicFormHTML += "</tr>";
+  dynamicFormHTML += "</thead>";
+  dynamicFormHTML += "<tbody>";
 
   for (let i = 0; i < participants; i++) {
-    dynamicFormHTML += "</tr>";
-    dynamicFormHTML += `<td><input type="text" placeholder="Participant name"></td>`;
+    dynamicFormHTML += "<tr>";
+    dynamicFormHTML += `<td><input class="participant-bl" id="participant${i}" type="text" placeholder="Participant name"></td>`;
 
     for (let b = 0; b < juries; b++) {
-      dynamicFormHTML += `<td><input id="input-NaN"  type="number" placeholder=""></td>`;
+      dynamicFormHTML += `<td><input class="pointinputs" step="10" id="point${i}_${b}" min="0" max="${elem}" type="number" placeholder=""></td>`;
     }
-    dynamicFormHTML += '<td><span id="totalScoreRow' + i + '">0</span></td>';
+
+    dynamicFormHTML += `<td><span class="total-rating" id="totalScoreRow${i}">0</span></td>`;
     dynamicFormHTML += "</tr>";
   }
+
+  dynamicFormHTML += "</tbody>";
   dynamicFormHTML += "</table>";
-  dynamicFormHTML +=
-    "<button  type='button' class='btn btn-danger'>Confirm</button>";
   dynamicFormHTML += "</div>";
   dynamicFormContainer.innerHTML = dynamicFormHTML;
-  dynamicFormContainer.appendChild(createSaveButton());
-}
+  // dynamicFormContainer.appendChild(createSaveButton());
 
-function totalScore(juryIndex, participantIndex, inputElement) {
-  const enteredValue = parseInt(inputElement.value, 10) || 0;
-  const totalScoreElement = document.getElementById(
-    "totalScoreRow" + juryIndex
-  );
-  totalScoreElement.innerText =
-    parseInt(totalScoreElement.innerText, 10) + enteredValue;
-}
+  function pointChange() {
+    for (let i = 0; i < participants; i++) {
+      const arrPoints = [];
+      for (let j = 0; j < juries; j++) {
+        const pointInput = document.getElementById(`point${i}_${j}`);
+        arrPoints.push(Number(pointInput.value) || 0);
+      }
 
-// const maxPoint = document.getElementById("maxPoints");
-// maxPoint.addEventListener("input", function (event) {
-//   let selectedValue = parseInt(event.target.value);
-//   const markInput = document.querySelectorAll(".mark");
-//   markInput.forEach(function (input) {
-//     if (parseInt(input.value) > selectedValue) {
-//       input.value = "";
-//     }
-//   });
-// });
-
-function upLimit() {
-  const maxPointSelect = document.getElementById("maxPoints");
-  const selectedValue = parseInt(maxPointSelect.value, 10) || 0;
-  const options = maxPointSelect.getElementsByTagName("option");
-
-  for (m = 1; m < options.length; m++) {
-    const optionValue = parseInt(options[m].value, 10);
-    options[m].disabled = optionValue > selectedValue;
+      const totalScoreRow = document.getElementById(`totalScoreRow${i}`);
+      totalScoreRow.innerHTML = `${arrPoints.reduce((a, b) => a + b, 0)}`;
+    }
   }
+
+  for (let i = 0; i < participants; i++) {
+    for (let j = 0; j < juries; j++) {
+      const pointInput = document.getElementById(`point${i}_${j}`);
+      pointInput.addEventListener("input", pointChange);
+    }
+  }
+  //DONT WORKING
+  // function autoRating() {
+  //   let arrRating = Array.from(document.querySelectorAll(".participant-bl"));
+  //   arrRating.sort(function (a, b) {
+  //     let ratingA = Number(a.querySelector(`.total-rating`).innerHTML);
+  //     let ratingB = Number(b.querySelector(`.total-rating`).innerHTML);
+  //     if (!isNaN(ratingA) && !isNaN(ratingB)) {
+  //       return ratingB - ratingA;
+  //     }
+  //     return 0;
+  //   });
+  //   for (let i = 0; i < participants.length; i++) {
+  //     dynamicFormContainer.appendChild(arrRating[i]);
+  //   }
+  // }
+  // autoRating();
+
+  // for (let i = 0; i < participants; i++) {
+  //   dynamicFormHTML += "<th data-index" + i + ">";
+  //   dynamicFormHTML += "</th>";
+  // }
+  // DONT WORKING
+  // arrRating.sort(function (a, b) {
+  //   let ratingA = Number(a.querySelector(".total-rating").innerHTML);
+  //   let ratingB = Number(b.querySelector(".total-rating").innerHTML);
+  //   let indexA = a.dataset.index;
+  //   let indexB = b.dataset.index;
+  //   if (!isNaN(ratingA) && !isNaN(ratingB)) {
+  //     return ratingB - ratingA || indexA - indexB;
+  //   }
+  //   return 0;
+  // });
+
+  // function createSaveButton() {
+  //   const saveButton = document.createElement("button");
+  //   saveButton.className = "btn btn-danger";
+  //   saveButton.type = "button";
+  //   saveButton.innerText = "Save";
+  //   saveButton.onclick = function () {
+  //     console.log("update...");
+  //   };
+
+  //   return saveButton;
+  // }
 }
